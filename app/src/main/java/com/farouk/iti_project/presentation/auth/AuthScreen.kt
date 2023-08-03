381a67b1a62f4bb8bbe9bd91079c8cc4f1e8d6b4
farouk <ahmed.farouk.334455@gmail.com>
2023-08-03 16:33:27 +0300

    task-3 design login screen with arabic and rotation support with features (hobbies CheckBox + Gender RadioBtn ) after btn clicked show all data in snack bar

--- app/src/main/java/com/farouk/iti_project/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/farouk/iti_project/presentation/auth/AuthScreen.kt
@@ -1,5 +1,6 @@
 package com.farouk.iti_project.presentation.auth
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,7 +10,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -27,51 +32,104 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.farouk.iti_project.R
+import com.farouk.iti_project.presentation.components.CheckBox
+import com.farouk.iti_project.presentation.components.CustomToast
+import com.farouk.iti_project.presentation.components.Gender
+import com.farouk.iti_project.presentation.components.GenderSelection
 import com.farouk.iti_project.presentation.components.OutLineTextFieldMail
 import com.farouk.iti_project.presentation.components.OutLineTextFieldPass
+import kotlinx.coroutines.delay
 
 @Composable
 fun AuthScreen() {
-    var mail by remember { mutableStateOf("Click a button") }
-    var mailAfterClick by remember { mutableStateOf("Click a button") }
+    var mail by remember { mutableStateOf("Empty") }
+    var pass by remember { mutableStateOf("Empty") }
+    var cliked by remember { mutableStateOf(false) }
     val gray = Color(0xFF3C3C3C)
     val blue = Color(0xFF2196F3)
+    var footballChecked by remember { mutableStateOf(false) }
+    var basketballChecked by remember { mutableStateOf(false) }
+    var volleyballChecked by remember { mutableStateOf(false) }
+    val hobbiesMap : MutableMap<String , Boolean> = mutableMapOf()
+    hobbiesMap["basketball"] = basketballChecked
+    hobbiesMap["football"] = footballChecked
+    hobbiesMap["volley"] = volleyballChecked
+
+    var selectedGender by remember { mutableStateOf(Gender.MALE) }
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(gray),
+            .background(gray)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
-        Text(text = stringResource(R.string.login_form), color = Color.White , fontWeight = FontWeight.Bold , fontSize = 50.sp)
+        Text(
+            text = stringResource(R.string.login_form),
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 50.sp
+        )
         Spacer(modifier = Modifier.height(30.dp))
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(start = 25.dp ,end = 45.dp)) {
-            Text(text = stringResource(R.string.username), color = Color.White , )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 25.dp, end = 45.dp)
+        ) {
+            Text(text = stringResource(R.string.username), color = Color.White)
             OutLineTextFieldMail(onNameChange = { newName ->
                 mail = newName
             }, txt = stringResource(R.string.enter_your_mail))
         }
         Spacer(modifier = Modifier.height(15.dp))
-        Row(verticalAlignment = Alignment.CenterVertically , modifier = Modifier.padding(start = 25.dp ,end = 45.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 25.dp, end = 45.dp)
+        ) {
             Text(text = stringResource(R.string.password), color = Color.White)
             OutLineTextFieldPass(onNameChange = { newName ->
-                mail = newName
+                pass = newName
             }, txt = stringResource(R.string.enter_your_password))
         }
-        Spacer(modifier = Modifier.height(30.dp))
-        Button(onClick = {
-            /*TODO*/
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(end = 75.dp)
+        ) {
+            Text(text = "favourite hobby ", color = Color.White)
+            Column {
+                CheckBox(onCheckChanges = { basketballChecked = it }, sportType = "basketball")
+                CheckBox(onCheckChanges = { footballChecked = it }, sportType = "Football")
+                CheckBox(onCheckChanges = { volleyballChecked = it }, sportType = "volley")
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(end = 125.dp)
+        ) {
+            Text(text = "Gender", color = Color.White)
+            GenderSelection(onGenderClicked = { selectedGender = it })
 
-        }, colors = ButtonDefaults.buttonColors(blue),
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = {
+                    cliked = true
+            }, colors = ButtonDefaults.buttonColors(blue),
             modifier = Modifier
                 .padding(start = 60.dp, end = 60.dp)
                 .wrapContentSize(align = Alignment.Center)
                 .fillMaxWidth()
-                ) {
+        ) {
             Text(text = stringResource(R.string.login_now), color = Color.White)
 
+        }
+        if (cliked){
+            val arrOFHobbies  = hobbiesMap.filterValues { it }.keys
+
+            CustomToast(message = "mail is $mail \b gender is $selectedGender \nhobbies is $arrOFHobbies" )
         }
 
     }
--- app/src/main/java/com/farouk/iti_project/presentation/components/AppComponents.kt
+++ app/src/main/java/com/farouk/iti_project/presentation/components/AppComponents.kt
@@ -1,11 +1,23 @@
 package com.farouk.iti_project.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -14,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -73,6 +86,94 @@ fun OutLineTextFieldPass(onNameChange: (String) -> Unit, txt: String) {
 fun Btn(onClick: () -> Unit, txt: String) {
     Button(onClick = onClick) {
         Text(text = txt)
+    }
+}
+
+@Composable
+fun CheckBox(onCheckChanges: (Boolean) -> Unit, sportType: String) {
+    var sport by remember {
+        mutableStateOf(false)
+    }
+    Column {
+        // Football checkbox
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = sport,
+                onCheckedChange = {
+                    sport = it
+                    onCheckChanges(sport)
+                },
+                colors = CheckboxDefaults.colors(
+                    checkmarkColor = blue,
+                    checkedColor = Color.White
+                )
+
+            )
+            Spacer(modifier = Modifier.width(3.dp))
+            Text(text = sportType, modifier = Modifier.width(110.dp), color = Color.White)
+        }
+
+    }
+}
+
+enum class Gender {
+    MALE,
+    FEMALE
+}
+
+@Composable
+fun GenderSelection(onGenderClicked: (Gender) -> Unit, modifier: Modifier = Modifier) {
+    var selectedGender by remember { mutableStateOf(Gender.MALE) }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        // Male radio button
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = selectedGender == Gender.MALE,
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = blue, disabledSelectedColor = Color.White
+                ),
+                onClick = { selectedGender = Gender.MALE }
+
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Male", color = Color.White)
+        }
+
+        // Female radio button
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = selectedGender == Gender.FEMALE,
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = blue, disabledSelectedColor = Color.White
+                ),
+                onClick = { selectedGender = Gender.FEMALE }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Female", color = Color.White)
+        }
+    }
+}
+
+
+@Composable
+fun CustomToast(message: String) {
+    Snackbar(
+        action = {
+
+        },
+        modifier = Modifier.padding(16.dp) ,
+
+    ) {
+        Text(text = message, color = Color.White)
     }
 }
 
